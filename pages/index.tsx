797e9dc155c4556787154e9591e89de43e41e0b9
Fred Matias <fredrmatias@gmail.com>
2023-05-07 01:05:18 +0100

    madeby

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,12 @@ import PostFeed from "@/components/posts/PostFeed";
 export default function Home() {
   return (
     <>
-      <Header label='Home' />
-      <Form placeholder="What's happening?"/>
+      <Header label="Home" />
+      <Form placeholder="What's happening?" />
       <PostFeed />
+      <footer style={{ textAlign: "center", margin: "2rem 0", color: 'lime' }}>
+        Made with love by fa-rm 2023
+      </footer>
     </>
-  )
+  );
 }
